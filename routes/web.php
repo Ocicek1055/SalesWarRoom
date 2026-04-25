1270c910ceeb9b2d183c3709d1adad6c45cbaee6
cicekozer <cicekozer@gmail.com>
2026-04-25 14:31:19 +0000

    refactor: Remove temporary setup route

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ContractorController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\StakeholderController;
 use App\Http\Controllers\SalesActivityController;
-
-// Geçici veritabanı kurulum ve önbellek temizleme rotası
-Route::get('/setup-database', function () {
-    try {
-        // Tüm önbellekleri temizle
-        Artisan::call('route:clear');
-        Artisan::call('config:clear');
-        Artisan::call('view:clear');
-        
-        // Veritabanı migrasyonunu çalıştır
-        Artisan::call('migrate', ['--force' => true]);
-        
-        return "Önbellek temizlendi ve veritabanı migrasyonları başarıyla çalıştırıldı.";
-    } catch (\Exception $e) {
-        return "İşlem sırasında bir hata oluştu: " . $e->getMessage();
-    }
-});
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
