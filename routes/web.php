3eb6c026831c19300cea592ae534901907fa076a
cicekozer <cicekozer@gmail.com>
2026-04-25 14:02:36 +0000

    fix: Remove fatal error in database config and clean up routes

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,6 @@ use App\Http\Controllers\ContractorController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\StakeholderController;
 use App\Http\Controllers\SalesActivityController;
-use Illuminate\Support\Facades\Artisan;
-
-// Gizli, geçici migrasyon rotası
-Route::get('/migrate-now-t8n3p5s', function () {
-    try {
-        Artisan::call('migrate', ['--force' => true]);
-        return '<h1>Veritabanı Tabloları Başarıyla Oluşturuldu!</h1><p>Bu pencereyi kapatıp uygulamayı tekrar ziyaret edebilirsiniz. Her şeyin artık çalışıyor olması gerekiyor.</p>';
-    } catch (Exception $e) {
-        return '<h1>Hata!</h1><p>Migrasyon çalıştırılırken bir hata oluştu: ' . $e->getMessage() . '</p>';
-    }
-});
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
