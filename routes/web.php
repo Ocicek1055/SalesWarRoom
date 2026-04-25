f0c3c9e076a94fefc64c2f58582a190728c0208d
cicekozer <cicekozer@gmail.com>
2026-04-25 14:10:33 +0000

    feat: Add temporary route to run migrations

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ContractorController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\StakeholderController;
 use App\Http\Controllers\SalesActivityController;
+
+// Geçici veritabanı kurulum rotası
+Route::get('/setup-database', function () {
+    try {
+        Artisan::call('migrate', ['--force' => true]);
+        return "Veritabanı migrasyonları başarıyla çalıştırıldı.";
+    } catch (\Exception $e) {
+        return "Migrasyon sırasında bir hata oluştu: " . $e->getMessage();
+    }
+});
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
